ca000d10898eb7c06deab349733f6812c7f7b893
Dan Lorenz <metalkid007@yahoo.com>
2023-11-06 21:04:51 -0600

    Added missing comments

--- src/ShieldMVVM/Converters/Converter.Methods.cs
+++ src/ShieldMVVM/Converters/Converter.Methods.cs
@@ -5,10 +5,20 @@ namespace CoreBTS.Maui.ShieldMVVM.Converters;
 
 public static partial class Converter
 {
+    /// <summary>
+    /// A converter that creates a command if given an Action.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <returns>An IValueConverter.</returns>
     public static IValueConverter<Action, ICommand> ConvertToCommand(this BindingConverter<Action, ICommand> _) =>
        Create(_,
            value => new RelayCommand(value));
 
+    /// <summary>
+    /// A converter that creates an async command if given a function that returns a task.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <returns>An IValueConverter.</returns>
     public static IValueConverter<Func<Task>, ICommand> ConvertToCommand(this BindingConverter<Func<Task>, ICommand> _) =>
        Create(_,
            value => new AsyncRelayCommand(value));
